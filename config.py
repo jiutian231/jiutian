16f0e270b2bab433325812a7086d99060aee35ff
jiutian231 <kingling22145@gmail.com>
2025-05-16 23:08:31 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 import os
-BOT_TOKEN = os.getenv("BOT_TOKEN")  # 替换为你的 BotFather 令牌
-CHANNEL_ID = os.getenv("CHANNEL_ID")
-# 客服用户名列表（自动轮询）
-CUSTOMER_SERVICE = ["@jt223326", "@aw21775"]
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")  # BotFather 生成的 Token
+GROUP_ID = int(os.getenv("GROUP_ID"))  # 你的话题群组的 ID，带负号
