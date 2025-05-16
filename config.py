b12826b6447854f444de00252428d601f88c1090
jiutian231 <kingling22145@gmail.com>
2025-05-16 22:11:40 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-BOT_TOKEN = "7025384539:AAHgRPFUe6BGtCPrv6SoVv7SF42t9pbVFOw"  # 替换为你的 BotFather 令牌
+BOT_TOKEN = "os.getenv("BOT_TOKEN")"  # 替换为你的 BotFather 令牌
 
 # 客服用户名列表（自动轮询）
 CUSTOMER_SERVICE = ["@jt223326", "@aw21775"]
